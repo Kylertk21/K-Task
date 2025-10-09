67545ebdab7a23a9568950040d4b8a7979f32fbc
kylertk21 <kylertk2102@gmail.com>
2025-10-09 14:15:27 -0600

    Basic data structure, add and search record methods stable

--- main.cpp
+++ main.cpp
@@ -1,16 +1,70 @@
 #include <iostream>
+#include <string>
+#include <unordered_map>
+#include <chrono>
+#include <random>
+#include <ranges>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+using namespace std;
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+class Data {
+public:
+    string name;
+    string description;
+    bool complete;
+    int priority;
+    string due_date; // TODO: convert to chrono::time
+    Data() {
+        name = "";
+        description = "";
+        complete = false;
+        priority = 0;
+        due_date = "";
     }
+};
+
+unordered_map<string, Data> tasks;
+
+void add_record(const string &name, const string &description, const bool complete,
+                const int priority, const string &due_date) {
+    Data new_task;
+    new_task.name = name;
+    new_task.description = description;
+    new_task.complete = complete;
+    new_task.priority = priority;
+    new_task.due_date = due_date;
+
+    tasks[new_task.name] = new_task;
+
+}
+
+void delete_record(string query) {
+
+}
+
+Data* search_records(string query) {
+    if (auto search = tasks.find(query); search != tasks.end()) {
+        return &search->second;
+    }
+    return nullptr;
+}
+
+chrono::system_clock::time_point convert_time(string time) {
+
+    return chrono::system_clock::now();
+}
+
+int main() {
+    add_record("test", "desc", true, 2, "12/02/2022");
+    const Data *query_data = search_records("test");
+    cout << "Name: " << query_data->name << endl;
+    cout << "Description: " << query_data->description << endl;
+    cout << "Complete: " << query_data->complete << endl;
+    cout << "Priority: " << query_data->priority << endl;
+    cout << "Due Date: " << query_data->due_date << endl;
 
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
+
+
+
